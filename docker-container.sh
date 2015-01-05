48f1da515c0b8c79800d424cf97f8d542397f378
core@core1 <core@core1>
2015-01-05 13:51:21 +0900

    restartシェル生成、imageバージョン未指定対応、インデント修正

--- docker-container.sh
+++ docker-container.sh
@@ -9,11 +9,16 @@ else
 	FULLPATH=$(cd `dirname $0`; pwd)/`basename $0`
 	DIR=`dirname $FULLPATH`
 	REPO=`basename $DIR`
-    REPO=`echo $REPO | sed -r "s/docker\-//g"`
-    IMAGE=$USER/$REPO
+	REPO=`echo $REPO | sed -r "s/docker\-//g"`
+	IMAGE=$USER/$REPO
 	if [ "$3" != "" ];then
-    	IMAGE=$IMAGE:$3
-    fi
+		IMAGE=$IMAGE:$3
+	else
+		VERSION=`docker images | grep "$IMAGE " | sort | tail -1 | awk '{print $2}'`
+		if [ "$VERSION" != "" ];then
+			IMAGE=$IMAGE:$VERSION
+		fi
+	fi
 	docker run -d --privileged --restart=always --name="$__FQDN__" --hostname="$__HOSTNAME__" \
 		-p $__PORT__:80 \
 		-v /var/www/:/var/www/ \
@@ -22,6 +27,13 @@ else
 		-v ${PWD}/export/nginx/conf.d/:/usr/local/nginx/conf.d/ \
 		-v ${PWD}/export/root/:/root/export/ \
 		$IMAGE
+
+	RESTART=./restart.sh
+	touch $RESTART
+	echo "docker rm -f $__FQDN__" >> $RESTART
+	echo "$0 $__FQDN__ $__PORT__" >> $RESTART
+	chmod +x $RESTART
+
 	BOOT=./container/docker-boot-$__HOSTNAME__.sh
 	BOOT_OFF=./container/docker-boot-off-$__HOSTNAME__.sh
 	REMOVE=./container/docker-remove-$__HOSTNAME__.sh
@@ -33,10 +45,10 @@ else
 	chmod +x $BOOT_OFF
 	chmod +x $REMOVE
 	$BOOT
-    NGINX_SRC=src/nginx-host.conf
-    NGINX_DST=dst/$__HOSTNAME__-$__PORT__.conf
-    cat $NGINX_SRC | sed -r "s/__HOSTNAME__/$__HOSTNAME__/g" > $NGINX_DST
-    sed -r -i "s/__FQDN__/$__FQDN__/g" $NGINX_DST
-    sed -r -i "s/__PORT__/$__PORT__/g" $NGINX_DST
+	NGINX_SRC=src/nginx-host.conf
+	NGINX_DST=dst/$__HOSTNAME__-$__PORT__.conf
+	cat $NGINX_SRC | sed -r "s/__HOSTNAME__/$__HOSTNAME__/g" > $NGINX_DST
+	sed -r -i "s/__FQDN__/$__FQDN__/g" $NGINX_DST
+	sed -r -i "s/__PORT__/$__PORT__/g" $NGINX_DST
 fi
 
